b3a5bc3243d1a0bc77767676501d4fcd765dd602
Pockykuma <pockykuma@gmail.com>
2020-07-23 01:22:54 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// PIXI.utils.sayHello();
+
+// const renderer = PIXI.autoDetectRenderer(  256,
+//      256,  {
+//     antialias: true,    // default: false
+//     transparent: false, // default: false
+//     resolution: 1       // default: 1)
+//      });
+
+//      document.getElementById('display').appendChild(renderer.view);
